2ff4aaf65bbebcf8fbaab029357ea7eafb7bed1c
Kimbo Chen <kimbo@semianalysis.com>
2025-08-13 04:35:28 +0000

    Updated commands for CW and NB clusters.

--- launch_70b_h200-nb.sh
+++ launch_70b_h200-nb.sh
@@ -13,10 +13,11 @@ cat > $JOB_SCRIPT << 'EOF'
 port=$(( 8888 + $PORT_OFFSET ))
 
 set -x
-vllm serve $MODEL --port $port \
---tensor-parallel-size $TP --distributed-executor-backend mp \
---dtype bfloat16 --quantization modelopt \
---max-num-seqs $CONC --max-model-len $MAX_MODEL_LEN --max-seq-len-to-capture $MAX_MODEL_LEN \
+vllm serve $MODEL --host 0.0.0.0 --port $port \
+--trust-remote-code --quantization modelopt --gpu-memory-utilization 0.9 \
+--pipeline-parallel-size 1 --tensor-parallel-size $TP --max-num-seqs $CONC --max-num-batched-tokens 8192 --max-model-len $MAX_MODEL_LEN \
+--enable-chunked-prefill --async-scheduling --no-enable-prefix-caching \
+--compilation-config '{"pass_config": {"enable_fi_allreduce_fusion": true}, "custom_ops": ["+rms_norm"], "level": 3}' \
 --disable-log-requests > /results/server_${SLURM_JOB_ID}.log 2>&1 &
 
 set +x
