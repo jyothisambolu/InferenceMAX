8fdc9e2afb476b9142c5aab6578df2fc38ead25b
Kimbo Chen <kimbo@semianalysis.com>
2025-08-14 19:37:05 +0000

    Fixed slurm command.

--- launch_dsv3_h200-nv.sh
+++ launch_dsv3_h200-nv.sh
@@ -45,7 +45,7 @@ python3 bench_serving/benchmark_serving.py \
 EOF
 
 set -x
-srun --partition=dgx-h200 --nodelist=dgx01-h200,dgx02-h200,dgx04-h200 --gres=gpu:$TP --exclusive \
+srun --partition=dgx-h200 --nodelist=dgx01-h200,dgx02-h200,dgx04-h200 --nodes=1 --exclusive --gres=gpu:$TP \
 --container-image=$IMAGE \
 --container-name=dsv3-$USER \
 --container-mounts=$GITHUB_WORKSPACE:/workspace/,$GHA_CACHE_DIR/hf_hub_cache/:$HF_HUB_CACHE \
