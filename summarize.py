e37ed204fb8c30d7f3c9373f957d84c232b973ae
Kimbo Chen <kimbo@semianalysis.com>
2025-08-20 21:24:02 +0000

    Added entry sorting.

--- summarize.py
+++ summarize.py
@@ -4,13 +4,12 @@ from pathlib import Path
 
 
 summary_header = f'''\
-## Benchmark Results
-
 | Hardware | TP | Conc | TTFT (ms) | TPOT (ms) | E2EL (s) | TPUT per GPU |
 | :-: | :-: | :-: | :-: | :-: | :-: | :-: |\
 '''
 print(summary_header)
 
+entries = []
 results_dir = Path(sys.argv[1])
 for result_path in results_dir.rglob(f'agg_{sys.argv[2]}_*.json'):
     with open(result_path) as f:
@@ -24,4 +23,8 @@ for result_path in results_dir.rglob(f'agg_{sys.argv[2]}_*.json'):
         f"| {result['median_e2el']:.4f} "
         f"| {result['tput_per_gpu']:.4f} |"
     )
+    entries.append(entry)
+
+entries.sort(key=lambda result: (result['hw'], result['tp'], result['conc']))
+for entry in entries:
     print(entry)
