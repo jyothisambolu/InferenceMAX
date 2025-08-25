b67d36f7ecff6bb58f12ba1f78a602997cb3f69f
Kimbo Chen <kimbo@semianalysis.com>
2025-08-25 04:20:04 +0000

    Fixed dumb metric typo.

--- benchmarks/70b_h100_slurm.sh
+++ benchmarks/70b_h100_slurm.sh
@@ -53,6 +53,6 @@ python3 bench_serving/benchmark_serving.py \
 --random-input-len $ISL --random-output-len $OSL --random-range-ratio $RANDOM_RANGE_RATIO \
 --num-prompts $(( $CONC * 10 )) --max-concurrency $CONC \
 --request-rate inf --ignore-eos \
---save-result --percentile-metrics 'ttft,tpot,itl,o2el' \
+--save-result --percentile-metrics 'ttft,tpot,itl,e2el' \
 --result-dir /workspace/ \
 --result-filename $RESULT_FILENAME.json
