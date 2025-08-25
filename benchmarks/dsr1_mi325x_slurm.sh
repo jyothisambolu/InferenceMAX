713bc7fefe8234209c96f82785b5302ef40cc9aa
Kimbo Chen <kimbo@semianalysis.com>
2025-08-25 05:04:33 +0000

    Fixed backslash bug.

--- benchmarks/dsr1_mi325x_slurm.sh
+++ benchmarks/dsr1_mi325x_slurm.sh
@@ -29,7 +29,7 @@ set -x
 git clone https://github.com/kimbochen/bench_serving.git
 python3 bench_serving/benchmark_serving.py \
 --model $MODEL --backend vllm \
---base-url http://0.0.0.0:\$PORT \
+--base-url http://0.0.0.0:$PORT \
 --dataset-name random \
 --random-input-len $ISL --random-output-len $OSL --random-range-ratio $RANDOM_RANGE_RATIO \
 --num-prompts $(( $CONC * 10 )) --max-concurrency $CONC \
