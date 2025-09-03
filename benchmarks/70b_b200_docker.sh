552b1b3b0bff88d13a3526e43c27be3b2e7527d5
Kimbo Chen <kimbo@semianalysis.com>
2025-09-03 04:00:51 +0000

    Fixed fusion flag.

--- benchmarks/70b_b200_docker.sh
+++ benchmarks/70b_b200_docker.sh
@@ -15,16 +15,16 @@ pip install --upgrade --no-deps nvidia-nccl-cu12==2.26.2.post1
 
 export VLLM_FLASHINFER_ALLREDUCE_FUSION_THRESHOLDS_MB='{"2":32,"4":32,"8":8}'
 
-FUSION_FLAG='{\
-"pass_config": {"enable_fi_allreduce_fusion": true, "enable_attn_fusion": true, "enable_noop": true},\
-"custom_ops": ["+quant_fp8", "+rms_norm"],\
-"cudagraph_mode": "FULL_DECODE_ONLY",\
-"splitting_ops": []\
-}'
+FUSION_FLAG='{'\
+'"pass_config": {"enable_fi_allreduce_fusion": true, "enable_attn_fusion": true, "enable_noop": true},'\
+'"custom_ops": ["+quant_fp8", "+rms_norm"],'\
+'"cudagraph_mode": "FULL_DECODE_ONLY",'\
+'"splitting_ops": []'\
+'}'
 
 vllm serve $MODEL --host 0.0.0.0 --port $PORT --trust-remote-code \
 --kv-cache-dtype fp8 --gpu-memory-utilization 0.9 \
 --pipeline-parallel-size 1 --tensor-parallel-size $TP --max-num-batched-tokens 8192 --max-num-seqs 512 --max-model-len $MAX_MODEL_LEN \
 --enable-chunked-prefill --async-scheduling --no-enable-prefix-caching \
---compilation-config $FUSION_FLAG \
+--compilation-config "$FUSION_FLAG" \
 --disable-log-requests
