7d9ab951e619c81f6b3ac8cfeeb0d2a553b2e34f
Kimbo Chen <kimbo@semianalysis.com>
2025-09-04 20:05:57 +0000

    Added explicit exit.

--- benchmarks/70b_mi355x_slurm.sh
+++ benchmarks/70b_mi355x_slurm.sh
@@ -42,3 +42,5 @@ python3 bench_serving/benchmark_serving.py \
 --request-rate inf --ignore-eos \
 --save-result --percentile-metrics "ttft,tpot,itl,e2el" \
 --result-dir /workspace/ --result-filename $RESULT_FILENAME.json
+
+exit
