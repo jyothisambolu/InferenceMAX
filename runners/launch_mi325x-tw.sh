09d44739e1f634904bfee2356a3f3041044a2da6
Kimbo Chen <kimbo@semianalysis.com>
2025-09-05 15:10:44 +0000

    Added container writable and remap root flag.

--- runners/launch_mi325x-tw.sh
+++ runners/launch_mi325x-tw.sh
@@ -16,6 +16,8 @@ srun --jobid=$JOB_ID \
 --container-image=$SQUASH_FILE \
 --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
 --container-mount-home \
+--container-writable \
+--container-remap-root \
 --container-workdir=/workspace/ \
 --no-container-entrypoint --export=ALL \
 bash benchmarks/${MODEL_CODE}_mi325x_slurm.sh
