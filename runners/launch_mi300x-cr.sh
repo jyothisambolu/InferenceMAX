4ab9ea7386f31bf9c60ac61f72b8a536deb16b7f
Kimbo Chen <kimbo@semianalysis.com>
2025-09-10 15:43:49 +0000

    Removed render group.

--- runners/launch_mi300x-cr.sh
+++ runners/launch_mi300x-cr.sh
@@ -14,7 +14,7 @@ docker network create $network_name
 set -x
 docker run --rm -d --ipc=host --shm-size=16g --network=$network_name --name=$server_name \
 --privileged --cap-add=CAP_SYS_ADMIN --device=/dev/kfd --device=/dev/dri --device=/dev/mem \
---group-add render --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
 -v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
 -v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
 -e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT \
