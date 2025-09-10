4b4144a4b1aa4978e27eee2d9dbce7f0952af371
Kimbo Chen <kimbo@semianalysis.com>
2025-09-10 16:10:09 +0000

    Updated DSR1 command.

--- benchmarks/dsr1_mi300x_docker.sh
+++ benchmarks/dsr1_mi300x_docker.sh
@@ -1,76 +1,25 @@
 #!/usr/bin/env bash
 
-# === Required Env Vars === 
+# ========= Required Env Vars =========
 # HF_TOKEN
 # HF_HUB_CACHE
-# IMAGE
 # MODEL
-# ISL
-# OSL
-# MAX_MODEL_LEN
-# RANDOM_RANGE_RATIO
+# PORT
 # TP
 # CONC
-# RESULT_FILENAME
-# PORT_OFFSET
-# HF_HUB_CACHE_MOUNT
-# GITHUB_WORKSPACE
+# MAX_MODEL_LEN
 
-sudo sh -c 'echo 0 > /proc/sys/kernel/numa_balancing'
+# Reference
+# https://rocm.docs.amd.com/en/docs-7.0-rc1/preview/benchmark-docker/inference-sglang-deepseek-r1-fp8.html#run-the-inference-benchmark
 
-while [ -n "$(docker ps -aq)" ]; do
-  sleep 1
-done
-
-network_name="bmk-net"
-server_name="bmk-server"
-client_name="bmk-client"
-port=8888
-
-docker network create $network_name
-
+export SGLANG_USE_AITER=1
 set -x
-docker run --rm -d --ipc host --shm-size=16g --network $network_name --name $server_name \
---privileged --cap-add=CAP_SYS_ADMIN --device=/dev/kfd --device=/dev/dri --device=/dev/mem \
---group-add render --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
--v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
--e HF_TOKEN=$HF_TOKEN -e HF_HUB_CACHE=$HF_HUB_CACHE -e VLLM_USE_TRITON_FLASH_ATTN=0 \
---entrypoint=python3 \
-$IMAGE \
--m sglang.launch_server --model-path $MODEL --host 0.0.0.0 --port $port --trust-remote-code \
---tp $TP --cuda-graph-max-bs $CONC --disable-radix-cache
-
-set +x
-while IFS= read -r line; do
-    printf '%s\n' "$line"
-    if [[ "$line" =~ [Ee][Rr][Rr][Oo][Rr] ]]; then
-        docker stop $server_name
-        exit 1
-    fi
-    if [[ "$line" == *"The server is fired up and ready to roll!"* ]]; then
-        break
-    fi
-done < <(docker logs -f --tail=0 $server_name 2>&1)
-
-git clone https://github.com/kimbochen/bench_serving.git
-
-set -x
-docker run --rm --network $network_name --name $client_name \
---privileged --cap-add=CAP_SYS_ADMIN --device=/dev/kfd --device=/dev/dri --device=/dev/mem \
---group-add render --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
--v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ -e HF_TOKEN=$HF_TOKEN -e PYTHONPYCACHEPREFIX=/tmp/pycache/ \
---entrypoint=python3 \
-$IMAGE \
-bench_serving/benchmark_serving.py \
---model $MODEL  --backend vllm --base-url http://$server_name:$port \
---dataset-name random \
---random-input-len $ISL --random-output-len $OSL --random-range-ratio $RANDOM_RANGE_RATIO \
---num-prompts $(( $CONC * 10 )) \
---max-concurrency $CONC \
---request-rate inf --ignore-eos \
---save-result --percentile-metrics "ttft,tpot,itl,e2el" \
---result-dir /workspace/ --result-filename $RESULT_FILENAME.json
-
-docker stop $server_name
-docker network rm $network_name
-
+python3 -m sglang.launch_server \
+--model-path=$MODEL --host=0.0.0.0 --port=$PORT --trust-remote-code \
+--tensor-parallel-size=$TP \
+--mem-fraction-static=0.8 \
+--cuda-graph-max-bs=$CONC \
+--chunked-prefill-size=196608 \
+--num-continuous-decode-steps=4 \
+--max-prefill-tokens=196608 \
+--disable-radix-cache
