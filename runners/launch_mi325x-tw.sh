f8da56cfed9c816ecac45f4a57b8825ea8fa5cbb
Kimbo Chen <kimbo@semianalysis.com>
2025-09-11 11:38:41 +0000

    Renabled enroot caching.

--- runners/launch_mi325x-tw.sh
+++ runners/launch_mi325x-tw.sh
@@ -11,7 +11,7 @@ set -x
 salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=128 --time=180 --no-shell
 JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
 
-# srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
+srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
 srun --jobid=$JOB_ID \
 --container-image=$IMAGE \
 --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
