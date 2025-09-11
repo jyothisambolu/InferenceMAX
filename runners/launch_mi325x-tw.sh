86aefaef0d7b765d13860b4382aa9f66522f004a
Kimbo Chen <kimbo@semianalysis.com>
2025-09-11 11:51:11 +0000

    Changed to squash file.

--- runners/launch_mi325x-tw.sh
+++ runners/launch_mi325x-tw.sh
@@ -13,7 +13,7 @@ JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
 
 srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
 srun --jobid=$JOB_ID \
---container-image=$IMAGE \
+--container-image=$SQUASH_FILE \
 --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
 --container-mount-home \
 --container-writable \
