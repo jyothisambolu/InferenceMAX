02d488c46f38ba7cd19539745ab2c5164f29f025
Kimbo Chen <kimbo@semianalysis.com>
2025-09-12 00:54:14 +0000

    Disable offline mode.

--- benchmarks/70b_mi300x_docker.sh
+++ benchmarks/70b_mi300x_docker.sh
@@ -12,7 +12,7 @@
 # Reference
 # https://rocm.docs.amd.com/en/docs-7.0-rc1/preview/benchmark-docker/inference-vllm-llama-3.3-70b-fp8.html#run-the-inference-benchmark
 
-export HF_HUB_OFFLINE=1
+# export HF_HUB_OFFLINE=1
 export VLLM_USE_V1=1
 export VLLM_V1_USE_PREFILL_DECODE_ATTENTION=1
 export AMDGCN_USE_BUFFER_OPS=1
