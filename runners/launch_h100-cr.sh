2b768839418c9c29f22295dc74c561bab88a06cb
Kimbo Chen <kimbo@semianalysis.com>
2025-09-13 22:05:47 +0000

    Updated model and precision multiplexing logic.

--- runners/launch_h100-cr.sh
+++ runners/launch_h100-cr.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 
-MODEL_CODE="${1%%_*}"
 HF_HUB_CACHE_MOUNT="/home/ubuntu/hf_hub_cache/"
-source benchmarks/${MODEL_CODE}_h100_docker.sh
+source benchmarks/${EXP_NAME%%_*}_${PRECISION}_h100_docker.sh
