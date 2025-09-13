35b3c08ea6e419cd4e33ceb9c3b09ed86946f2c1
Kimbo Chen <kimbo@semianalysis.com>
2025-09-13 06:23:39 +0000

    Added DSR1 MI355X.

---
+++ benchmarks/dsr1_fp4_mi355x_slurm.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# ========= Required Env Vars =========
+# HF_TOKEN
+# HF_HUB_CACHE
+# MODEL
+# ISL
+# OSL
+# MAX_MODEL_LEN
+# RANDOM_RANGE_RATIO
+# TP
+# CONC
+# PORT
+# RESULT_FILENAME
+
+SERVER_LOG=$(mktemp /tmp/server-XXXXXX.log)
+
+set -x
+python3 -m sglang.launch_server --model-path=$MODEL --trust-remote-code \
+--host=0.0.0.0 --port=$PORT \
+--tensor-parallel-size=$TP \
+--chunked-prefill-size=196608 \
+--mem-fraction-static=0.8 \
+--disable-radix-cache \
+--num-continuous-decode-steps=4 \
+--max-prefill-tokens=196608 \
+--cuda-graph-max-bs=$CONC \
+> $SERVER_LOG 2>&1 &
+
+set +x
+while IFS= read -r line; do
+    printf '%s\n' "$line"
+    if [[ "$line" == *"The server is fired up and ready to roll"* ]]; then
+        break
+    fi
+done < <(tail -F -n0 "$SERVER_LOG")
+
+set -x
+git clone https://github.com/kimbochen/bench_serving.git
+python3 bench_serving/benchmark_serving.py \
+--model $MODEL --backend vllm \
+--base-url "http://0.0.0.0:$PORT" \
+--dataset-name random \
+--random-input-len $ISL --random-output-len $OSL --random-range-ratio $RANDOM_RANGE_RATIO \
+--num-prompts $(( $CONC * 10 )) --max-concurrency $CONC \
+--request-rate inf --ignore-eos \
+--save-result --percentile-metrics "ttft,tpot,itl,e2el" \
+--result-dir /workspace/ --result-filename $RESULT_FILENAME.json
