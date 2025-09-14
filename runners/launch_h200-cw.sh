954d31eae18bc2f29ad9c0ae1cdbde6e8e15b1b1
Kimbo Chen <kimbo@semianalysis.com>
2025-09-14 21:55:20 +0000

    Fixed squash file path.

--- runners/launch_h200-cw.sh
+++ runners/launch_h200-cw.sh
@@ -7,7 +7,7 @@ MODEL_CODE="${EXP_NAME%%_*}"
 FRAMEWORK_SUFFIX=$([[ "$FRAMEWORK" == "trt" ]] && printf '_trt' || printf '')
 
 PARTITION="h200"
-SQUASH_FILE="/raid/image_${MODEL_CODE}_h200${FRAMEWORK_SUFFIX}.sqsh"
+SQUASH_FILE="/mnt/vast/image_${MODEL_CODE}_h200${FRAMEWORK_SUFFIX}.sqsh"
 
 salloc --partition=$PARTITION --gres=gpu:$TP --exclusive --time=180 --no-shell
 JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
