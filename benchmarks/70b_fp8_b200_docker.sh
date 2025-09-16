5f91fd91d3321eca546638a0b5ffa6d16a4b5e58
Kimbo Chen <kimbo@semianalysis.com>
2025-09-16 19:49:18 +0000

    Updated B200 vLLM 70B for docker.

--- benchmarks/70b_fp8_b200_docker.sh
+++ benchmarks/70b_fp8_b200_docker.sh
@@ -9,10 +9,7 @@
 # CONC
 # MAX_MODEL_LEN
 
-pip install flashinfer-python==0.3.0
-pip install --upgrade --no-deps nvidia-nccl-cu12==2.26.2.post1
-
-export VLLM_FLASHINFER_ALLREDUCE_FUSION_THRESHOLDS_MB='{"2":32,"4":32,"8":8}'
+pip install -q datasets pandas
 
 FUSION_FLAG='{'\
 '"pass_config": {"enable_fi_allreduce_fusion": true, "enable_attn_fusion": true, "enable_noop": true},'\
@@ -20,11 +17,24 @@ FUSION_FLAG='{'\
 '"cudagraph_mode": "FULL_DECODE_ONLY",'\
 '"splitting_ops": []'\
 '}'
+cat > config.yaml <<-EOF
+kv-cache-dtype: fp8
+compilation-config: \'$FUSION_FLAG\'
+async-scheduling: true
+no-enable-prefix-caching: true
+max-num-batched-tokens: 8192
+max-model-len: $MAX_MODEL_LEN
+EOF
+
+cat config.yaml  # Debugging
+
+export VLLM_FLASHINFER_ALLREDUCE_FUSION_THRESHOLDS_MB='{"2":32,"4":32,"8":8}'
+export PYTHONNOUSERSITE=1
 
 set -x
-vllm serve $MODEL --host 0.0.0.0 --port $PORT --trust-remote-code \
---kv-cache-dtype fp8 --gpu-memory-utilization 0.9 \
---pipeline-parallel-size 1 --tensor-parallel-size $TP --max-num-batched-tokens 8192 --max-num-seqs 512 --max-model-len $MAX_MODEL_LEN \
---enable-chunked-prefill --async-scheduling --no-enable-prefix-caching \
---compilation-config "$FUSION_FLAG" \
+vllm serve $MODEL --host=0.0.0.0 --port=$PORT \
+--gpu-memory-utilization=0.9 \
+--tensor-parallel-size=$TP \
+--max-num-seqs=512 \
+--config=config.yaml \
 --disable-log-requests
