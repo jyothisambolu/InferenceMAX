8a43975180402e7a162f7f2ea9aa9684e4468305
Kimbo Chen <kimbo@semianalysis.com>
2025-09-16 04:23:59 +0000

    Changed enroot caching naming scheme.

--- runners/launch_b200-nv.sh
+++ runners/launch_b200-nv.sh
@@ -7,7 +7,7 @@ MODEL_CODE="${EXP_NAME%%_*}"
 FRAMEWORK_SUFFIX=$([[ "$FRAMEWORK" == "trt" ]] && printf '_trt' || printf '')
 
 PARTITION="dgx-b200"
-SQUASH_FILE="/raid/image_${MODEL_CODE}_b200${FRAMEWORK_SUFFIX}.sqsh"
+SQUASH_FILE="/raid/squash/$(echo "$IMAGE" | sed 's/[\/:@]/_/g').sqsh"
 
 salloc --partition=$PARTITION --gres=gpu:$TP --exclusive --time=180 --no-shell
 JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
--- runners/launch_h100-cw.sh
+++ runners/launch_h100-cw.sh
@@ -5,7 +5,7 @@ export HF_HUB_CACHE_MOUNT="/mnt/vast/hf_hub_cache/"
 export PORT_OFFSET=${USER: -1}
 
 PARTITION="h100"
-SQUASH_FILE="/mnt/vast/squash/image_${MODEL_CODE}_h100.sqsh"
+SQUASH_FILE="/mnt/vast/squash/$(echo "$IMAGE" | sed 's/[\/:@]/_/g').sqsh"
 
 salloc --partition=$PARTITION --gres=gpu:$TP --exclusive --time=180 --no-shell
 JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
