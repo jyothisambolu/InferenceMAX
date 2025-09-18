a8cd04b32e8d3e62aea4d1462a0b6f6684faba08
Jorge Parada <jparada@amd.com>
2025-09-18 10:52:38 -0700

    Update 70b_fp8_mi300x_docker.sh

--- benchmarks/70b_fp8_mi300x_docker.sh
+++ benchmarks/70b_fp8_mi300x_docker.sh
@@ -33,7 +33,6 @@ vllm serve $MODEL --port=$PORT \
 --max-model-len=$MAX_MODEL_LEN \
 --max-seq-len-to-capture=$MAX_MODEL_LEN \
 --max-num-seqs=$CONC \
---max-num-batched-tokens=$(( $CONC * 128 )) \
 --no-enable-prefix-caching \
 --async-scheduling \
 --disable-log-requests
