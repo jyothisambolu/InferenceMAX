851ba910d6027a73d35d8b0d9dd0dd989d5aad4c
kimbochen <40498137+kimbochen@users.noreply.github.com>
2025-09-21 13:24:25 -0400

    Convert OCI Cluster to Docker Setup (#33)
    * Changed to docker setup.
    
    * Added missing MI300X SGLang image.
    
    * Allow stats collection always run even when cancelled.
    
    * Replaced squash file naming.
    
    * Test fix of enroot import error.
    
    ---------
    
    Co-authored-by: Kimbo Chen <kimbo@semianalysis.com>

--- runners/launch_mi300x-oci.sh
+++ runners/launch_mi300x-oci.sh
@@ -1,24 +1,53 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-export HF_HUB_CACHE_MOUNT="$HOME/hf_hub_cache/"
+HF_HUB_CACHE_MOUNT="$HOME/hf_hub_cache/"
+PORT=8888
 
-PARTITION="compute"
-SQUASH_FILE="$HOME/squash/$(echo "$IMAGE" | sed 's/[\/:@#]/_/g').sqsh"
+network_name="bmk-net"
+server_name="bmk-server"
+client_name="bmk-client"
+
+docker network create $network_name
 
 set -x
-salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=224 --time=180 --no-shell
-JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
+docker run --rm -d --ipc=host --shm-size=16g --network=$network_name --name=$server_name \
+--privileged --cap-add=CAP_SYS_ADMIN --device=/dev/kfd --device=/dev/dri --device=/dev/mem \
+--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+-v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
+-v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
+-e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT \
+--entrypoint=/bin/bash \
+$IMAGE \
+benchmarks/"${EXP_NAME%%_*}_${PRECISION}_mi300x_docker.sh"
 
-srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
-srun --jobid=$JOB_ID \
---container-image=$SQUASH_FILE \
---container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
---container-mount-home \
---container-writable \
---container-remap-root \
---container-workdir=/workspace/ \
---no-container-entrypoint \
---export=ALL \
-bash benchmarks/${EXP_NAME%%_*}_${PRECISION}_mi300x_slurm.sh
+set +x
+while IFS= read -r line; do
+    printf '%s\n' "$line"
+    if [[ "$line" =~ Application\ startup\ complete ]]; then
+        break
+    fi
+done < <(docker logs -f --tail=0 $server_name 2>&1)
 
-scancel $JOB_ID
+git clone https://github.com/kimbochen/bench_serving.git
+
+set -x
+docker run --rm --network=$network_name --name=$client_name \
+-v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
+-e HF_TOKEN -e PYTHONPYCACHEPREFIX=/tmp/pycache/ \
+--entrypoint=python3 \
+$IMAGE \
+bench_serving/benchmark_serving.py \
+--model=$MODEL --backend=vllm --base-url=http://$server_name:$PORT \
+--dataset-name=random \
+--random-input-len=$ISL --random-output-len=$OSL --random-range-ratio=$RANDOM_RANGE_RATIO \
+--num-prompts=$(( $CONC * 10 )) \
+--max-concurrency=$CONC \
+--request-rate=inf --ignore-eos \
+--save-result --percentile-metrics="ttft,tpot,itl,e2el" \
+--result-dir=/workspace/ --result-filename=$RESULT_FILENAME.json
+
+while [ -n "$(docker ps -aq)" ]; do
+    docker stop $server_name
+    docker network rm $network_name
+    sleep 5
+done
--- runners/launch_mi325x-tw.sh
+++ runners/launch_mi325x-tw.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-MODEL_CODE="${EXP_NAME%%_*}"
 export HF_HUB_CACHE_MOUNT="/home/hf_hub_cache/"
 export PORT_OFFSET=${USER: -1}
 
 PARTITION="gpuworker"
-SQUASH_FILE="/home/.tw/slinky/.cache/squash/image_${MODEL_CODE}_mi325x.sqsh"
+SQUASH_FILE="/home/.tw/slinky/.cache/squash/$(echo "$IMAGE" | sed 's/[\/:@#]/_/g').sqsh"
 
 set -x
 salloc --partition=$PARTITION --gres=gpu:$TP --cpus-per-task=128 --time=180 --no-shell
 JOB_ID=$(squeue -u $USER -h -o %A | head -n1)
 
-srun --jobid=$JOB_ID bash -c "enroot import -o $SQUASH_FILE docker://$IMAGE"
+srun --jobid=$JOB_ID bash -c "sudo enroot import -o $SQUASH_FILE docker://$IMAGE"
 srun --jobid=$JOB_ID \
 --container-image=$SQUASH_FILE \
 --container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
@@ -20,6 +19,6 @@ srun --jobid=$JOB_ID \
 --container-remap-root \
 --container-workdir=/workspace/ \
 --no-container-entrypoint --export=ALL \
-bash benchmarks/${MODEL_CODE}_${PRECISION}_mi325x_slurm.sh
+bash benchmarks/${EXP_NAME%%_*}_${PRECISION}_mi325x_slurm.sh
 
 scancel $JOB_ID
