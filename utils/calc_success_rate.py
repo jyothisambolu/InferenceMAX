26200a870f5b4826207ad4c39315eeeebf00259d
kimbochen <40498137+kimbochen@users.noreply.github.com>
2025-09-21 23:35:54 -0400

    Reduce B200 Runs - More Stuff (#36)
    * Copied slurm command to docker.
    
    * Reduced B200 TP sizes.
    
    * Copied command from slurm script.
    
    * Copied command from slurm script.
    
    * Assign B200 TRT runs to b200-trt runners.
    
    * Updated H200 and B200 total runs.
    
    ---------
    
    Co-authored-by: Kimbo Chen <kimbo@semianalysis.com>

--- utils/calc_success_rate.py
+++ utils/calc_success_rate.py
@@ -4,15 +4,21 @@ from pathlib import Path
 
 n_iosl = 3   # [1k1k, 8k1k, 1k8k]
 n_concs = 5  # [4, 8, 16, 32, 64]
-custom_n_concs = 7  # [4, 8, 16, 32, 64, 128, 256]
+
+# H200: (70b-tp: [1, 2, 4, 8], dsr1-tp: [8], gptoss-tp: [1, 2, 4, 8]) x [vllm/sglang, trt] + 70b-tp x trt extra conc: [128]
+h200_runs = (4 + 1 + 4) * 2 * n_iosl * n_concs + 4 * n_iosl * 1
+
+# B200: ((70b-tp: [1, 8], dsr1-tp: [8]) x [fp4, fp8], gptoss-tp: [1, 8]) x [vllm/sglang, trt] + 7b-tp x trt extra conc: [128, 256]
+b200_runs = ((2 + 1) * 2 + 2) * 2 * n_iosl * n_concs + 2 * n_iosl * 2
+
 total_runs = {
-    'h100': (3 + 4) * n_iosl * n_concs,                # 70b-tp: [2, 4, 8], gptoss-tp: [1, 2, 4, 8]
-    'h200': (4 + 1 + 4) * 2 * n_iosl * n_concs,        # (70b-tp: [1, 2, 4, 8], dsr1-tp: [8], gptoss-tp: [1, 2, 4, 8]) x [vllm/sglang, trt]
-    'b200': ((2 + 1) * 2 + 2) * 2 * n_iosl * n_concs + 1 * n_iosl * custom_n_concs,  # ((70b-tp: [1, 8], dsr1-tp: [8]) x [fp4, fp8], gptoss-tp: [1, 8]) x [vllm/sglang, trt], dsr1-tpfp4: [4]
-    'mi300x': (4 + 1 + 4) * n_iosl * n_concs,          # 70b-tp: [1, 2, 4, 8], dsr1-tp: [8], gptoss-tp: [1, 2, 4, 8]
-    'mi325x': (4 + 1 + 4) * n_iosl * n_concs,          # 70b-tp: [1, 2, 4, 8], dsr1-tp: [8], gptoss-tp: [1, 2, 4, 8]
-    'mi355x': ((4 + 1) * 2 + 4) * n_iosl * n_concs,    # (70b-tp: [1, 2, 4, 8], dsr1-tp: [8]) x [fp4, fp8], gptoss-tp: [1, 2, 4, 8]
-    'gb200': 45,                                       # 45 runs in complete sweep for gb200 dsr1 fp4 trtllm
+    'h100': (3 + 4) * n_iosl * n_concs,              # 70b-tp: [2, 4, 8], gptoss-tp: [1, 2, 4, 8]
+    'h200': h200_runs,
+    'b200': b200_runs,
+    'mi300x': (4 + 1 + 4) * n_iosl * n_concs,        # 70b-tp: [1, 2, 4, 8], dsr1-tp: [8], gptoss-tp: [1, 2, 4, 8]
+    'mi325x': (4 + 1 + 4) * n_iosl * n_concs,        # 70b-tp: [1, 2, 4, 8], dsr1-tp: [8], gptoss-tp: [1, 2, 4, 8]
+    'mi355x': ((4 + 1) * 2 + 2) * n_iosl * n_concs,  # (70b-tp: [1, 2, 4, 8], dsr1-tp: [8]) x [fp4, fp8], gptoss-tp: [1, 2, 4, 8]
+    'gb200': 45,                                     # 45 runs in complete sweep for gb200 dsr1 fp4 trtllm
 }
 success_runs = {'h100': 0, 'h200': 0, 'b200': 0, 'mi300x': 0, 'mi325x': 0, 'mi355x': 0, 'gb200': 0}
 
