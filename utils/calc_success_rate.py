22d715f49b34674e14ec0debbb392df11100b477
Kimbo Chen <kimbo@semianalysis.com>
2025-09-22 03:42:30 +0000

    Fixed B200 calculation error.

--- utils/calc_success_rate.py
+++ utils/calc_success_rate.py
@@ -8,8 +8,8 @@ n_concs = 5  # [4, 8, 16, 32, 64]
 # H200: (70b-tp: [1, 2, 4, 8], dsr1-tp: [8], gptoss-tp: [1, 2, 4, 8]) x [vllm/sglang, trt] + 70b-tp x trt extra conc: [128]
 h200_runs = (4 + 1 + 4) * 2 * n_iosl * n_concs + 4 * n_iosl * 1
 
-# B200: ((70b-tp: [1, 8], dsr1-tp: [8]) x [fp4, fp8], gptoss-tp: [1, 8]) x [vllm/sglang, trt] + 7b-tp x trt extra conc: [128, 256]
-b200_runs = ((2 + 1) * 2 + 2) * 2 * n_iosl * n_concs + 2 * n_iosl * 2
+# B200: ((70b-tp: [1, 8], dsr1-tp: [8]) x [fp4, fp8], gptoss-tp: [1, 8]) x [vllm/sglang, trt] + 7b-tp x [fp4, fp8] x trt extra conc: [128, 256]
+b200_runs = ((2 + 1) * 2 + 2) * 2 * n_iosl * n_concs + 2 * 2 * n_iosl * 2
 
 total_runs = {
     'h100': (3 + 4) * n_iosl * n_concs,              # 70b-tp: [2, 4, 8], gptoss-tp: [1, 2, 4, 8]
