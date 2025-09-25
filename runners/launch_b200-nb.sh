a5b7b95ef186ccdc78abe9f009050ec030185fdb
kimbochen <40498137+kimbochen@users.noreply.github.com>
2025-09-25 10:09:26 -0400

    Add More B200 Nodes (#51)
    * Test new NB B200 nodes.
    
    * Set port offset.
    
    * Added new runner.
    
    * Added custom enroot cache path.
    
    * Changed cache to data.
    
    * Removed enroot env var.
    
    * Test unique container names.
    
    ---------
    
    Co-authored-by: Kimbo Chen <kimbo@semianalysis.com>

---
+++ runners/launch_b200-nb.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+HF_HUB_CACHE_MOUNT="/root/hf_hub_cache-${USER: -1}/"
+PARTITION="main"
+FRAMEWORK_SUFFIX=$([[ "$FRAMEWORK" == "trt" ]] && printf '_trt' || printf '')
+
+set -x
+srun --partition=$PARTITION --gres=gpu:$TP --exclusive \
+--container-image=$IMAGE \
+--container-name=$(echo "$IMAGE" | sed 's/[\/:@#]/_/g')-${USER: -1} \
+--container-mounts=$GITHUB_WORKSPACE:/workspace/,$HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
+--container-mount-home \
+--container-workdir=/workspace/ \
+--no-container-entrypoint --export=ALL,PORT_OFFSET=${USER: -1} \
+bash benchmarks/${EXP_NAME%%_*}_${PRECISION}_b200${FRAMEWORK_SUFFIX}_slurm.sh
