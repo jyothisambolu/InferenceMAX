a25544f73cbaaeaeb442e3bff1a453821efb8a15
J PS <jparada@amd.com>
2025-10-02 16:17:04 -0700

    reduce gpu mem req for config 8k 1k fp4 con64 (#80)

--- benchmarks/dsr1_fp4_mi355x_docker.sh
+++ benchmarks/dsr1_fp4_mi355x_docker.sh
@@ -11,14 +11,21 @@
 # PORT
 export SGLANG_USE_AITER=1
 
+PREFILL_SIZE=196608
+if [[ "$ISL" == "8192" && "$OSL" == "1024" ]]; then
+	if [[ "$CONC" -gt "32" ]]; then
+		PREFILL_SIZE=32768
+	fi
+fi
+
 set -x
 python3 -m sglang.launch_server --model-path=$MODEL --trust-remote-code \
 --host=0.0.0.0 --port=$PORT \
 --tensor-parallel-size=$TP \
---chunked-prefill-size=196608 \
+--chunked-prefill-size=$PREFILL_SIZE \
 --mem-fraction-static=0.8 \
 --disable-radix-cache \
 --num-continuous-decode-steps=4 \
---max-prefill-tokens=196608 \
+--max-prefill-tokens=$PREFILL_SIZE \
 --cuda-graph-max-bs=128
 
