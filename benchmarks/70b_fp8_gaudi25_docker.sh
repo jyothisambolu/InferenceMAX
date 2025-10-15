a0fa1bfef0186caf62f41ef0d2332d1368b8a9a2
SURESH NAMPALLI <snampalli@habana.ai>
2025-10-15 23:35:35 +0300

    updated with fp8 changes

--- benchmarks/70b_fp8_gaudi25_docker.sh
+++ benchmarks/70b_fp8_gaudi25_docker.sh
@@ -12,13 +12,16 @@
 pip install -q datasets pandas
 
 cat > config.yaml << EOF
-kv-cache-dtype: fp8
+kv-cache-dtype: fp8_inc
 async-scheduling: true
 no-enable-prefix-caching: true
+quantization: inc
+weights-load-device: cpu
 max-num-batched-tokens: 8192
 max-model-len: 10240
 EOF
 
+export QUANT_CONFIG=/software/inc/llama-3.3-70b-instruct-$TP/maxabs_quant_g3.json
 export PYTHONNOUSERSITE=1
 
 vllm serve $MODEL --host=0.0.0.0 --port=$PORT \
